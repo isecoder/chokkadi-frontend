5fd5e0c50f65e08374535a6a7f1a4b70be00a6db
Shreekara B <shreekarab5@gmail.com>
2025-01-26 00:32:32 +0530

    Fixed gallery section

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -87,22 +87,22 @@ export default function Gallery(): JSX.Element {
   };
 
   return (
-    <div className="relative px-4 md:px-8 lg:px-16 mb-10">
+    <div className="relative px-4 md:px-8 lg:px-16 mb-10 bg-yellow-100">
       {loading && <LoadingSpinner />}
 
       {/* Centered Title for the Temple */}
-      <div className="text-center mt-10 mb-4">
-        <h1 className="text-3xl font-bold">Shrirama TEMPLE</h1>
+      <div className="text-center mb-4">
+        <h1 className="text-3xl font-bold text-green-800">Shrirama Temple Chokkadi</h1>
       </div>
 
       {/* Image Gallery Section */}
-      <h2 className="text-xl font-semibold mt-10 mb-4">PHOTOS</h2>
+      <h2 className="text-xl font-semibold mb-4 text-green-700">PHOTOS</h2>
       {images.length > 0 ? (
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-2">
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
           {images.map((image, index) => (
             <div
               key={image.image_id}
-              className="relative w-full h-48 cursor-pointer overflow-hidden group"
+              className="relative w-full h-48 cursor-pointer overflow-hidden group bg-white rounded-lg shadow-lg transition-shadow duration-300 hover:shadow-xl"
               onClick={() => openImageModal(index)}
             >
               {/* Image */}
@@ -111,13 +111,11 @@ export default function Gallery(): JSX.Element {
                 alt={image.alt_text}
                 fill
                 loading="lazy"
-                className="object-cover transition-transform duration-300 hover:scale-105"
+                className="object-cover rounded-lg transition-transform duration-300 hover:scale-105"
               />
               {/* Title Hover Overlay */}
-              <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-                <p className="text-white text-lg font-semibold">
-                  {image.title}
-                </p>
+              <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 transition-opacity duration-300 group-hover:opacity-100 rounded-lg">
+                <p className="text-white text-lg font-semibold">{image.title}</p>
               </div>
             </div>
           ))}
